e3881d03b1115115cd6359e8e8be1aaf1be7a97d
Naviz Khoja <naviz.khoja@eventbeep.com>
2023-07-31 16:54:16 +0530

    feat : added func outlines

---
+++ database/databaseConnection.go
@@ -0,0 +1,40 @@
+package database
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func DBInstance() *mongo.Client {
+	MongoDb := "mongodb://localhost:27017"
+	fmt.Print(MongoDb)
+
+	clientOptions := options.Client().ApplyURI(MongoDb)
+	client, err := mongo.Connect(context.Background(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to Mongo")
+	return client
+}
+
+var Client *mongo.Client = DBInstance()
+
+func OpenCollection(client *mongo.Client, collectionName string) *mongo.Collection {
+	collection := client.Database("restro").Collection(collectionName)
+	return collection
+}
